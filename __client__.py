b0d5b196a45beee9d700046323892ad304702a61
ExDefiler <soundwave137010@gmail.com>
2021-10-31 01:19:31 +0500

    Sixth milestone.
    All protocol commands are implemented, but without taking care of race conditions.

--- __client__.py
+++ __client__.py
@@ -1,64 +1,102 @@
 from socket import *
 from threading import *
+import os
 
+CLIENT_FOLDER = r'D:\python\CLIENT'
 HOST = gethostbyname(gethostname())
 PORT_SEND = 2021
 PORT_RCV = 2022
 BUF_SIZE = 1024
 print_screen = Lock()
+running = True
+connected = False
+os.chdir(CLIENT_FOLDER)
+
+
+def configure_directory():
+    lf = os.listdir()
+    for filename in lf:
+        if ' ' in filename:
+            mod_filename = filename.replace(' ', '_')
+            os.rename(filename, mod_filename)
+
+
+def print_response(socket_object):
+
+    response = socket_object.recv(BUF_SIZE).decode('ascii')
+    print_screen.acquire()
+    print(response)
+    print_screen.release()
+    return response
 
 
 def message_thread():
+    global connected
 
     rcv_socket = socket(AF_INET, SOCK_STREAM)
     rcv_socket.bind((HOST, PORT_RCV))
     rcv_socket.listen()
     conn, address = rcv_socket.accept()
 
-    while True:
-        try:
-            message = conn.recv(BUF_SIZE).decode()
-        except error as e:
-            print_screen.acquire()
-            print(e)
-            print_screen.release()
-            conn.close()
-            rcv_socket.close()
-            break
+    while connected:
 
-        if message:
+        message = ''
+        while '\n' not in message:
+
+            packet = conn.recv(1).decode('ascii')
+
+            if packet:
+                message += packet
+
+            else:
+                connected = False
+                break
+
+        if message == "DISCONNECT\n":
+            connected = False
+
+        elif message == "MESSAGE\n":
+            message_length = ''
+            while ' ' not in message_length:
+                char = conn.recv(1).decode('ascii')
+                message_length += char
+            message_length = int(message_length[:-1])
+            message = conn.recv(message_length).decode('ascii')
             print_screen.acquire()
             print(message)
             print_screen.release()
 
-        else:
-            conn.close()
-            rcv_socket.close()
-            break
+    conn.close()
+    rcv_socket.close()
 
 
-def protocol_thread(sock, username):
+def protocol_thread(client_socket, username):
     global running
+    global connected
 
-    with sock:
+    with client_socket:
 
-        sock.send(f"CONNECT {username}".encode())
-        response = sock.recv(BUF_SIZE)
-        print_screen.acquire()
-        print(response.decode())
-        print_screen.release()
+        client_socket.send(f"CONNECT {username}\n".encode('ascii'))
+        response = print_response(client_socket)
+        if response == "ERROR\n":
+            connected = False
+            return
 
-        while True:
+        while connected:
             print_screen.acquire()
-            command = input("Type a command: ").split()
+            command = input("[CONNECTED] Type a command: ").split()
             print_screen.release()
+
+            if not connected:
+                continue
 
             if len(command) == 1:
 
                 if command[0] == "disconnect":
-                    sock.send("DISCONNECT".encode())
-                    response = sock.recv(BUF_SIZE).decode()
-                    if response == 'OK':
+                    client_socket.send("DISCONNECT\n".encode('ascii'))
+                    response = client_socket.recv(BUF_SIZE).decode('ascii')
+                    if response == 'OK\n':
+                        connected = False
                         break
                     else:
                         print_screen.acquire()
@@ -66,107 +104,147 @@ def protocol_thread(sock, username):
                         print_screen.release()
 
                 elif command[0] == "lu":
-                    sock.send("LU".encode())
-                    response = sock.recv(BUF_SIZE).decode()
-                    print_screen.acquire()
-                    print(response)
-                    print_screen.release()
+                    client_socket.send("LU\n".encode('ascii'))
+                    print_response(client_socket)
 
                 elif command[0] == "lf":
-                    sock.send("LF".encode())
-                    response = sock.recv(BUF_SIZE).decode()
-                    print_screen.acquire()
-                    print(response)
-                    print_screen.release()
+                    client_socket.send("LF\n".encode('ascii'))
+                    print_response(client_socket)
 
                 elif command[0] == "quit":
                     print_screen.acquire()
                     print("Program is finishing.")
                     print_screen.release()
+                    connected = False
                     running = False
                     break
 
                 else:
                     print_screen.acquire()
-                    print("Wrong command! Try another pattern.")
+                    print("Wrong command! Try another pattern.\n")
                     print_screen.release()
 
             elif len(command) == 2:
 
                 if command[0] == "read":
-                    sock.send(f"READ {command[1]}".encode())
-                    response = sock.recv(BUF_SIZE).decode()
-                    print_screen.acquire()
-                    print(response)
-                    print_screen.release()
+                    if command[1] in os.listdir():
+                        print(f"[FILENAME ERROR] The directory already contains a file with name: {command[1]}\n")
+                        continue
+                    client_socket.send(f"READ {command[1]}\n".encode('ascii'))
+                    response = print_response(client_socket)
+                    if response == "OK\n":
+                        filesize = ''
+                        while ' ' not in filesize:
+                            char = client_socket.recv(1).decode('ascii')
+                            filesize += char
+                        filesize = int(filesize[:-1])
+                        bytes_read = client_socket.recv(filesize)
+                        with open(command[1], "wb") as f:
+                            f.write(bytes_read)
 
                 elif command[0] == "write":
-                    sock.send(f"WRITE {command[1]}".encode())
-                    response = sock.recv(BUF_SIZE).decode()
-                    print_screen.acquire()
-                    print(response)
-                    print_screen.release()
+                    if command[1] in os.listdir():
+                        client_socket.send(f"WRITE {command[1]}\n".encode('ascii'))
+                        response = print_response(client_socket)
+                        if response == "OK\n":
+                            filesize = os.path.getsize(command[1])
+                            client_socket.send(f'{str(filesize)} '.encode('ascii'))
+                            with open(command[1], 'rb') as f:
+                                filecontent = f.read()
+                                client_socket.sendall(filecontent)
+                            print_response(client_socket)
+                    else:
+                        print(f"[FILENAME ERROR] The directory contains no file with name: {command[1]}\n")
 
                 elif command[0] == "overread":
-                    sock.send(f"OVERREAD {command[1]}".encode())
-                    response = sock.recv(BUF_SIZE).decode()
-                    print_screen.acquire()
-                    print(response)
-                    print_screen.release()
+                    client_socket.send(f"OVERREAD {command[1]}\n".encode('ascii'))
+                    response = print_response(client_socket)
+                    if response == "OK\n":
+                        filesize = ''
+                        while ' ' not in filesize:
+                            char = client_socket.recv(1).decode('ascii')
+                            filesize += char
+                        filesize = int(filesize[:-1])
+                        bytes_read = client_socket.recv(filesize)
+                        with open(command[1], "wb") as f:
+                            f.write(bytes_read)
 
                 elif command[0] == "overwrite":
-                    sock.send(f"OVERWRITE {command[1]}".encode())
-                    response = sock.recv(BUF_SIZE).decode()
-                    print_screen.acquire()
-                    print(response)
-                    print_screen.release()
+                    if command[1] in os.listdir():
+                        client_socket.send(f"OVERWRITE {command[1]}\n".encode('ascii'))
+                        response = print_response(client_socket)
+                        if response == "OK\n":
+                            filesize = os.path.getsize(command[1])
+                            client_socket.send(f'{str(filesize)} '.encode('ascii'))
+                            with open(command[1], 'rb') as f:
+                                filecontent = f.read()
+                                client_socket.sendall(filecontent)
+                            print_response(client_socket)
+                    else:
+                        print(f"[FILENAME ERROR] The directory contains no file with name: {command[1]}\n")
 
                 else:
                     print_screen.acquire()
-                    print("Wrong command! Try another pattern.")
+                    print("Wrong command! Try another pattern.\n")
                     print_screen.release()
 
             elif command[0] == "appendfile" and len(command) == 3:
+                if command[1] not in os.listdir():
+                    print(f"[FILENAME ERROR] The directory contains no file with name: {command[1]}\n")
+                    continue
 
-                sock.send(f"APPENDFILE {command[1]} {command[2]}".encode())
-                response = sock.recv(BUF_SIZE)
-                print_screen.acquire()
-                print(response.decode())
-                print_screen.release()
+                client_socket.send(f"APPENDFILE {command[1]} {command[2]}\n".encode('ascii'))
+                response = print_response(client_socket)
+                if response == "OK\n":
+                    filesize = os.path.getsize(command[1])
+                    client_socket.send(f'{str(filesize)} '.encode('ascii'))
+                    with open(command[1], 'rb') as f:
+                        filecontent = f.read()
+                        client_socket.sendall(filecontent)
+                    print_response(client_socket)
 
             elif command[0] == "send" and len(command) >= 3:
-                message = ""
-                for i in range(2, len(command)):
-                    message = message + command[i] + ' '
-                message = message.rstrip()
-                if message[0] == '"':
-                    message = message[1:]
-                if message[-1] == '"':
-                    message = message[:-1]
-                sock.send(f"MESSAGE {command[1]}\n{len(message)} {message}".encode())
-                response = sock.recv(BUF_SIZE)
-                print_screen.acquire()
-                print(response.decode())
-                print_screen.release()
+
+                if command[2].startswith('"') and command[-1].endswith('"'):
+                    message = ''
+                    for i in range(2, len(command)):
+                        message = message + command[i] + ' '
+                    message = message.strip('" ')
+                    client_socket.send(f"MESSAGE {command[1]}\n{len(message.encode('ascii'))} {message}".encode('ascii'))
+                    print_response(client_socket)
+
+                else:
+                    print('The message to be sent must come inside double quotation marks: "..."')
 
             elif command[0] == "append" and len(command) >= 3:
-                sock.send(f"APPEND {command[-1]}".encode())
-                response = sock.recv(BUF_SIZE)
-                print_screen.acquire()
-                print(response.decode())
-                print_screen.release()
+
+                if command[1].startswith('"') and command[-2].endswith('"'):
+                    client_socket.send(f"APPEND {command[-1]}\n".encode())
+                    response = print_response(client_socket)
+                    if response == "OK\n":
+                        content = ''
+                        for word in command[1:-1]:
+                            content += word
+                            content += ' '
+                        content = content.strip('" ')
+                        content = content.encode('ascii')
+                        client_socket.send(f"{str(len(content))} ".encode('ascii'))
+                        client_socket.send(content)
+                        print_response(client_socket)
+
+                else:
+                    print('The content to be appended must come inside double quotation marks: "..."')
 
             else:
                 print_screen.acquire()
-                print("Wrong command! Try another pattern.")
+                print("Wrong command! Try another pattern.\n")
                 print_screen.release()
 
 
-running = True
-
+configure_directory()
 while running:
 
-    instruction = input("Type a command: ").split()
+    instruction = input("[NOT CONNECTED] Type a command: ").split()
 
     if instruction[0] == "quit":
         print("Program is finishing.")
@@ -177,17 +255,17 @@ while running:
         s = socket(AF_INET, SOCK_STREAM)
         try:
             s.connect((instruction[2], PORT_SEND))
-        except error as err:
+            connected = True
+        except OSError as err:
             print(err)
             continue
 
-        t1 = Thread(target=protocol_thread, args=(s, instruction[1]))
-        t2 = Thread(target=message_thread)
-
-        t2.start()
-        t1.start()
-        t1.join()
-        t2.join()
+        prt_thread = Thread(target=protocol_thread, args=(s, instruction[1]))
+        msg_thread = Thread(target=message_thread)
+        msg_thread.start()
+        prt_thread.start()
+        prt_thread.join()
+        msg_thread.join()
 
     else:
-        print("Wrong command! Correct pattern is 'connect USERNAME IP_ADDRESS'")
+        print("Wrong command! Correct pattern is 'connect USERNAME IP_ADDRESS'\n")
--- __server__.py
+++ __server__.py
@@ -1,112 +1,229 @@
 from socket import *
 from threading import *
+import os
 
+SERVER_FOLDER = r'D:\python\SERVER'
 SERVER = gethostbyname(gethostname())
 PORT = 2021
 CLIENT_PORT = 2022
 BUF_SIZE = 1024
-CONNECTED_USERS = []
+CONNECTED_USERS = {}
 mutex = Lock()
 
+os.chdir(SERVER_FOLDER)
 server = socket(AF_INET, SOCK_STREAM)
 server.bind((SERVER, PORT))
+
+
+def configure_directory():
+    lf = os.listdir()
+    for filename in lf:
+        if ' ' in filename:
+            mod_filename = filename.replace(' ', '_')
+            os.rename(filename, mod_filename)
 
 
 def client_handler(conn, address):
 
     connected = True
     msg_socket = socket(AF_INET, SOCK_STREAM)
-
-    username = conn.recv(BUF_SIZE).decode().split()[1]
-    print(f"[NEW CONNECTION] User {username}: {address} connected.")
-    conn.send("OK".encode())
-
-    mutex.acquire()
-    CONNECTED_USERS.append(username)
-    mutex.release()
-
     try:
         msg_socket.connect((address[0], CLIENT_PORT))
     except error:
         print("Failed to connect to client's message receiving port.")
+        conn.send("ERROR\n".encode('ascii'))
+        conn.close()
+        msg_socket.close()
+        return
+
+    username = conn.recv(BUF_SIZE).decode('ascii').split()[1]
+    mutex.acquire()
+    if username in CONNECTED_USERS:
+        conn.send("ERROR\n".encode('ascii'))
+        conn.close()
+        msg_socket.close()
+        mutex.release()
+        return
+    mutex.release()
+
+    print(f"[NEW CONNECTION] User {username}: {address} connected.")
+    conn.send("OK\n".encode('ascii'))
+
+    mutex.acquire()
+    CONNECTED_USERS[username] = [msg_socket, Lock()]
+    mutex.release()
 
     while connected:
 
         try:
-            command = conn.recv(BUF_SIZE).decode()
-        except error as err:
+            command = ''
+            while '\n' not in command:
+                packet = conn.recv(1).decode('ascii')
+                if packet:
+                    command += packet
+                else:
+                    connected = False
+                    break
+        except OSError as err:
             print(err)
             break
 
-        if command:
-            command = command.split()
+        command = command.split()
 
-            if len(command) == 1:
+        if len(command) == 1:
 
-                if command[0] == "DISCONNECT":
-                    conn.send("OK".encode())
-                    conn.close()
-                    msg_socket.close()
-                    print(f"[END OF SESSION] User {username} disconnected.")
-                    connected = False
-
-                elif command[0] == "LU":
-                    lu = ''
-                    mutex.acquire()
-                    for user in CONNECTED_USERS:
-                        lu += user
-                        lu += ' '
-                    mutex.release()
-                    lu = lu[:-1] + '\n'
-                    conn.send(lu.encode())
-
-                elif command[0] == "LF":
-                    conn.send("Available in the future. Stay tuned!\n".encode())
+            if command[0] == "DISCONNECT":
+                if not CONNECTED_USERS[username][1].locked():
+                    CONNECTED_USERS[username][1].acquire()
+                    conn.send("OK\n".encode('ascii'))
+                    break
 
                 else:
-                    conn.send("Violating protocols. Being disconnected from server.".encode())
-                    conn.close()
-                    msg_socket.close()
-                    connected = False
+                    conn.send("ERROR\n".encode('ascii'))
 
-            elif len(command) == 2:
+            elif command[0] == "LU":
+                lu = ''
+                mutex.acquire()
+                for user in CONNECTED_USERS:
+                    lu += user
+                    lu += ' '
+                mutex.release()
+                lu = lu[:-1] + '\n'
+                conn.send(lu.encode('ascii'))
 
-                if command[0] == "READ":
-                    conn.send("OK".encode())
-
-                elif command[0] == "WRITE":
-                    conn.send("OK".encode())
-
-                elif command[0] == "OVERREAD":
-                    conn.send("OK".encode())
-
-                elif command[0] == "OVERWRITE":
-                    conn.send("OK".encode())
-
-                elif command[0] == "APPEND":
-                    conn.send("OK".encode())
-
-            elif len(command) == 3 and command[0] == "APPENDFILE":
-                conn.send("OK".encode())
-
-            elif command[0] == "MESSAGE" and len(command) >= 4:
-                msg_socket.send("Message delivered".encode())
-                conn.send("OK".encode())
+            elif command[0] == "LF":
+                lf = ''
+                for filename in os.listdir():
+                    lf += filename
+                    lf += ' '
+                lf = lf[:-1] + '\n'
+                conn.send(lf.encode('ascii'))
 
             else:
-                conn.send("Violating protocols. Being disconnected from server.".encode())
-                conn.close()
-                msg_socket.close()
-                connected = False
+                conn.send("ERROR\n".encode('ascii'))
+
+        elif len(command) == 2:
+
+            if command[0] == "READ":
+                if command[1] in os.listdir() and os.path.isfile(command[1]):
+                    conn.send("OK\n".encode('ascii'))
+                    filesize = os.path.getsize(command[1])
+                    conn.send(f'{str(filesize)} '.encode('ascii'))
+                    with open(command[1], 'rb') as f:
+                        filecontent = f.read()
+                        conn.sendall(filecontent)
+                else:
+                    conn.send("ERROR\n".encode('ascii'))
+
+            elif command[0] == "WRITE":
+                if command[1] in os.listdir():
+                    conn.send("ERROR\n".encode('ascii'))
+                else:
+                    conn.send("OK\n".encode('ascii'))
+                    filesize = ''
+                    while ' ' not in filesize:
+                        char = conn.recv(1).decode('ascii')
+                        filesize += char
+                    filesize = int(filesize[:-1])
+                    bytes_read = conn.recv(filesize)
+                    with open(command[1], "wb") as f:
+                        f.write(bytes_read)
+                    if os.path.getsize(command[1]) == filesize:
+                        conn.send("OK\n".encode('ascii'))
+                    else:
+                        conn.send("ERROR\n".encode('ascii'))
+
+            elif command[0] == "OVERREAD":
+                if command[1] in os.listdir() and os.path.isfile(command[1]):
+                    conn.send("OK\n".encode('ascii'))
+                    filesize = os.path.getsize(command[1])
+                    conn.send(f'{str(filesize)} '.encode('ascii'))
+                    with open(command[1], 'rb') as f:
+                        filecontent = f.read()
+                        conn.sendall(filecontent)
+                else:
+                    conn.send("ERROR\n".encode('ascii'))
+
+            elif command[0] == "OVERWRITE":
+                conn.send("OK\n".encode('ascii'))
+                filesize = ''
+                while ' ' not in filesize:
+                    char = conn.recv(1).decode('ascii')
+                    filesize += char
+                filesize = int(filesize[:-1])
+                bytes_read = conn.recv(filesize)
+                with open(command[1], "wb") as f:
+                    f.write(bytes_read)
+                if os.path.getsize(command[1]) == filesize:
+                    conn.send("OK\n".encode('ascii'))
+                else:
+                    conn.send("ERROR\n".encode('ascii'))
+
+            elif command[0] == "APPEND":
+                if command[1] in os.listdir() and os.path.isfile(command[1]):
+                    conn.send("OK\n".encode('ascii'))
+                    old_filesize = os.path.getsize(command[1])
+                    filesize = ''
+                    while ' ' not in filesize:
+                        char = conn.recv(1).decode('ascii')
+                        filesize += char
+                    filesize = int(filesize[:-1])
+                    content = conn.recv(filesize).decode('ascii')
+                    with open(command[1], "a") as f:
+                        f.write(content)
+                    if os.path.getsize(command[1]) == filesize + old_filesize:
+                        conn.send("OK\n".encode('ascii'))
+                    else:
+                        conn.send("ERROR\n".encode('ascii'))
+
+                else:
+                    conn.send("ERROR\n".encode('ascii'))
+
+            elif command[0] == "MESSAGE":
+                message_length = ''
+                while ' ' not in message_length:
+                    char = conn.recv(1).decode('ascii')
+                    message_length += char
+                message_length = int(message_length[:-1])
+                message = conn.recv(message_length)
+                mutex.acquire()
+                if command[1] in CONNECTED_USERS:
+                    CONNECTED_USERS[command[1]][0].send(f"MESSAGE\n{message_length} ".encode('ascii'))
+                    CONNECTED_USERS[command[1]][0].send(message)
+                    conn.send("OK\n".encode('ascii'))
+                else:
+                    conn.send("ERROR\n".encode('ascii'))
+                mutex.release()
+
+        elif len(command) == 3 and command[0] == "APPENDFILE":
+            if command[2] in os.listdir():
+                conn.send("OK\n".encode('ascii'))
+                old_filesize = os.path.getsize(command[2])
+                filesize = ''
+                while ' ' not in filesize:
+                    char = conn.recv(1).decode('ascii')
+                    filesize += char
+                filesize = int(filesize[:-1])
+                bytes_read = conn.recv(filesize)
+                with open(command[2], "ab") as f:
+                    f.write(bytes_read)
+                if os.path.getsize(command[2]) == filesize + old_filesize:
+                    conn.send("OK\n".encode('ascii'))
+                else:
+                    conn.send("ERROR\n".encode('ascii'))
+
+            else:
+                conn.send("ERROR\n".encode('ascii'))
 
         else:
-            print(f"[END OF SESSION] User {username} disconnected.")
-            conn.close()
-            msg_socket.close()
-            connected = False
+            conn.send("ERROR\n".encode('ascii'))
+
+    conn.close()
+    msg_socket.close()
+    print(f"[END OF SESSION] User {username} disconnected.")
 
     mutex.acquire()
-    CONNECTED_USERS.remove(username)
+    CONNECTED_USERS.pop(username)
     mutex.release()
 
 
@@ -122,5 +239,6 @@ def start():
         thread.start()
 
 
+configure_directory()
 print("[STARTING] Server is starting...")
 start()
